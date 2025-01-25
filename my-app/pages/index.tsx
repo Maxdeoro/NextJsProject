f817304bed615d60ae27f86b1bed9297055daea4
Maxdeoro <maxdeoro@gmail.com>
2025-01-26 00:49:19 +0300

    create component header

--- my-app/pages/index.tsx
+++ my-app/pages/index.tsx
@@ -1,8 +1,14 @@
 import { JSX } from 'react';
+import { Htag } from '../components/htag/Htag';
 
 export default function Home(): JSX.Element {
   return (
-    <div>Hello World!</div>
+    <div>
+      <Htag tag='h1'>Text h1</Htag>
+      <Htag tag='h2'>Text h2</Htag>
+      <Htag tag='h3'>Text h3</Htag>
+    </div>
   )
 };
+
 
