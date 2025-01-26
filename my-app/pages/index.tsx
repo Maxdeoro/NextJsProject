4d110cee2fe4ba2cf5b5131a0893f7c43104aeb1
Maxdeoro <maxdeoro@gmail.com>
2025-01-27 00:06:26 +0300

    component Button with classnames

--- my-app/pages/index.tsx
+++ my-app/pages/index.tsx
@@ -1,5 +1,6 @@
 import { JSX } from 'react';
 import { Htag } from '../components/htag/Htag';
+import { Button } from '../components/button/Button';
 
 export default function Home(): JSX.Element {
   return (
@@ -7,6 +8,8 @@ export default function Home(): JSX.Element {
       <Htag tag='h1'>Text h1</Htag>
       <Htag tag='h2'>Text h2</Htag>
       <Htag tag='h3'>Text h3</Htag>
+      <Button appearance='primary'>BTN text</Button>
+      <Button appearance='ghost'>BTN text</Button>
     </div>
   )
 };
