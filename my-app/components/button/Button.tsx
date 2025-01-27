6e66156e8528c063bb111babc7312d24dc2490eb
Maxdeoro <maxdeoro@gmail.com>
2025-01-28 01:04:03 +0300

    set svg as ArrowIcon

--- my-app/components/button/Button.tsx
+++ my-app/components/button/Button.tsx
@@ -1,5 +1,6 @@
 import { JSX } from "react";
 import {ButtonProps} from './Button.props';
+import ArrowIcon from './arrow.svg';
 import styles from './Button.module.css';
 import cn from "classnames";
 
@@ -13,7 +14,10 @@ export const Button = ({appearance, arrow='none', children, className, ...props}
         >
             {children}
             {arrow !== 'none' && <span className={cn(styles.arrow, { 
-                [styles.down]: arrow == 'down'})}>{'>'}</span>}
+                [styles.down]: arrow == 'down'})}>
+                        {/* {'>'} */}
+                        <ArrowIcon/>
+                    </span>}
         </button>
     );
 };
