117d09cf275d9c11bd49d05e549002cb4c09dcd5
Maxdeoro <maxdeoro@gmail.com>
2025-02-10 01:13:07 +0300

    create css-grid Footer

--- my-app/layout/footer/Footer.tsx
+++ my-app/layout/footer/Footer.tsx
@@ -1,10 +1,15 @@
 import { JSX } from "react";
 import { FooterProps } from "./Footer.props";
+import cn from 'classnames';
+import styles from './Footer.module.css';
+import { format } from 'date-fns';
 
-export const Footer = ({...props}: FooterProps): JSX.Element => {
+export const Footer = ({className, ...props}: FooterProps): JSX.Element => {
     return (
-        <div {...props}>
-            FOOTER
-        </div>
+        <footer className={cn(className, styles.footer)} {...props}>
+            <div className={styles.sec1}>MySite © 2024 - {format(new Date(), 'yyyy')} All rights reserved</div>
+            <a href='#' target='_blank'>User Agreement</a>
+            <a href='#' target='_blank'>Privecy Policy</a>
+        </footer>
     );
 };
